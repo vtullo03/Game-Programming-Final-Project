9c8d147e2e57cb05fa9181b5b2e52febd54b39b1
vtullo03 <vitotullo430@gmail.com>
2023-12-15 13:16:12 -0500

    Pledge added

--- FinalProject/FinalProject/main.cpp
+++ FinalProject/FinalProject/main.cpp
@@ -1,3 +1,12 @@
+/**
+* Author: Vitoria Tullo
+* Assignment: Animalus
+* Date due: 2023-12-15, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
 
 #define GL_SILENCE_DEPRECATION
 #define GL_GLEXT_PROTOTYPES 1
